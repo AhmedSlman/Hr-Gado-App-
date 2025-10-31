7cda5d91120c61db9e4b64b324582368c5da5344
Ahmed Slman <ahmedslman151@gmail.com>
2025-10-31 18:34:52 +0200

    refactor(employees_view): remove unused import for employee_card_widget

--- lib/features/account/presentation/views/employees_view.dart
+++ lib/features/account/presentation/views/employees_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hr_app/core/common/widgets/custom_app_bar.dart';
 import 'package:hr_app/features/account/presentation/components/employees_grid_view_section.dart';
-import 'package:hr_app/features/account/presentation/widgets/employee_card_widget.dart';
 
 class EmployeesView extends StatelessWidget {
   const EmployeesView({super.key});
