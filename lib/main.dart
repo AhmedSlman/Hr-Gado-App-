b6e528bd2b94cac5cccd3373618aa78fc61b85cc
karimelsayed24 <karimelsa38@gmail.com>
2026-01-06 15:57:04 +0200

    fix: Update permissions in AndroidManifest.xml and increment version in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'core/locator/locator_setup.dart';
+
 import 'app.dart';
 import 'core/general/my_bloc_observer.dart';
+import 'core/locator/locator_setup.dart';
 
 Future<void> main() async {
   runZonedGuarded(() async {
